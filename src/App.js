d781e61f893c695706476f319a1c18e59ad21ce4
JongCHONG <yonhoa27@hotmail.fr>
2022-01-31 15:06:51 +0100

    suivre les conseils de benoit

--- src/App.js
+++ src/App.js
@@ -2,31 +2,17 @@ import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
-import HomePage from "./pages/HomePage"
-import LoginPage from "./pages/LoginPage"
-import SignUpPage from "./pages/SignUpPage"
-import ContactPage from "./pages/ContactPage"
-import CreateTeamPage from "./pages/CreateTeamPage"
-import UserProfilePage from "./pages/UserProfilePage"
-import UsersPage from "./pages/UsersPage"
-import TeamProfilePage from "./pages/TeamProfilePage"
-import TeamsPage from "./pages/TeamsPage"
-import NotFound from "./pages/NotFound"
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/createteam" element={<CreateTeamPage />} />
-        <Route path="/userprofile" element={<UserProfilePage />} />
-        <Route path="/users" element={<UsersPage />} />
-        <Route path="/teamprofile" element={<TeamProfilePage />} />
-        <Route path="/teams" element={<TeamsPage />} />
+        <Route exact path="/" element={<Home />} />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/Separator.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Separator = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Separator
