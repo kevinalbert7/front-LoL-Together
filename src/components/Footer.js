8cdd7a0b9c1386c4ec97137282c23acc105f092e
unknown <jochong27@gmail.com>
2022-02-02 00:10:58 +0100

    homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import { RiTwitterLine, RiFacebookLine, RiInstagramLine, RiGithubLine } from 'react-icons/ri'
+
+import styled from 'styled-components'
+
+import GridFooter from './GridFooter'
+import Title from './Title'
+
+const FooterBackground = styled.div`
+  background-color: black;
+  padding: 3%;
+`
+const FooterBackgroundRow = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <FooterBackground>
+      <FooterBackgroundRow>
+        <GridFooter title="Joueur" text1="Créez votre compte" text2="Rechercher des équipes" />
+        <GridFooter title="Equipe" text1="Créez votre équipe" text2="Rechercher des joueurs" />
+        <GridFooter title="Ressources" text1="Conditions d'utilisations" text2="Contact" />
+      </FooterBackgroundRow>
+      <div className='d-flex align-items-center my-5 flex-column'>
+        <Title text='Made by LoL Together Team' size='18' />
+        <div className='d-flex justify-content-center my-3'>
+          <RiTwitterLine  color="white" size="3%"/>
+          <RiFacebookLine  color="white" size="3%"/>
+          <RiInstagramLine  color="white" size="3%"/>
+          <RiGithubLine  color="white" size="3%"/>
+        </div>
+      </div>
+    </FooterBackground>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,65 @@
 import React from 'react'
 
-import img from '../images/home-background.png'
+import backgroundImage from '../images/home-background.png'
+
 import homeWave from '../images/home-wave.svg'
-import { RiUserLine } from 'react-icons/ri';
+
+import { RiUserLine, RiTeamLine } from 'react-icons/ri'
 
 import styled from 'styled-components'
 
 import Nav from '../components/Nav'
+import Logo from '../components/Logo'
+import Title from '../components/Title'
+import Footer from '../components/Footer'
 
 const Discover = styled.div`
-  background-image: url(${img});
+  background-image: url(${backgroundImage});
   height: 100vh;
   background-repeat: no-repeat;
   background-size: cover;
-  z-index: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  positive: relative;
+    @media (max-width: 1024px) {
+      background-size: 100%;
+    }
 `
 const HomeWave = styled.div`
   background-image: url(${homeWave});
-  z-index: 2;
+  position: absolute;
+  top: 600px;
   min-width: 100%;
-  max-width: 1440px;
-  background-repeat: no-repeat;
-  background-size: cover;
   height: 270px;
+    @media (max-width: 1024px) {
+      background-size: 100%;
+    }
+`
+const DiscoverDiv = styled.div`
+  right: 10%;
+  top: 10%;
+  position: absolute;
+  font-size: 36px;
+  width: 300px;
+  .p2 {
+    font-size: 15px;
+  }
 `
 const Middle = styled.div`
-  background-color: #201226;
+  background-color: #361F40;
+  padding: 50px;
+`
+const HomeCard = styled.div`
+  border: 1px solid white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+const Description = styled.div`
+  margin: 20px;
+`
+const Step = styled.div`
+  border: 1px solid white;
+
 `
 
 const Home = () => {  
@@ -36,19 +67,73 @@ const Home = () => {
     <>
       <Discover>
         <Nav />
+        <DiscoverDiv>
+          <Logo />
+          <p>Recrutez des jouers pour ton équipe, ou fais- toi recruter.</p>
+          <p className='p2'>Parce que jouer ensemble c'est aussi passé de bons moments.</p>
+          <Title text='Découvrir' size='72' />
+        </DiscoverDiv>
         <HomeWave/>
       </Discover>
       <Middle>
         <div className='container'>
-        <RiUserLine />
-
-          <p>TEST</p>
-          <p>TEST</p>
-          <p>TEST</p>
-          <p>TEST</p>
-          <p>TEST</p>
+          <div className='row'>
+            <div className='col-6'>
+              <HomeCard>
+                <RiUserLine size='50%' />
+                <p>Vous cherchez un joueur?</p>
+              </HomeCard>
+              <Description>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </Description>
+              <div className='row'>
+                <div className='col-4'>
+                  <Step>
+                    Etape 1
+                  </Step>
+                </div>
+                <div className='col-4'>
+                <Step>
+                    Etape 2
+                  </Step>
+                </div>
+                <div className='col-4'>
+                <Step>
+                    Etape 3
+                </Step>
+                </div>
+              </div>
+            </div>
+            <div className='col-6'>
+                <HomeCard>
+                  <RiTeamLine size='50%' />
+                  <p>Vous cherchez un équipe?</p>
+                </HomeCard>
+              <Description>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </Description>
+              <div className='row'>
+                <div className='col-4'>
+                  <Step>
+                    Etape 1
+                  </Step>
+                </div>
+                <div className='col-4'>
+                <Step>
+                    Etape 2
+                  </Step>
+                </div>
+                <div className='col-4'>
+                <Step>
+                    Etape 3
+                </Step>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </Middle>
+      <Footer />
     </>
   )
 }
