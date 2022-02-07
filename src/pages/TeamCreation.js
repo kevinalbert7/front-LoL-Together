6571571f551170917b395eb87fda684221191aff
kevinalbert7 <20ko@live.fr>
2022-02-08 00:53:34 +0100

    creation de team

--- src/pages/TeamCreation.js
+++ src/pages/TeamCreation.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect, useContext } from 'react'
+import { MultiSelect } from "react-multi-select-component"
+import Select from 'react-select'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { useNavigate } from 'react-router-dom'
 
 import { 
-  getRegion,
-  optionsRegions, 
-} from '../api/filter'
+    getRegion, 
+    getLanguages, 
+    getSelectedInfos, 
+    getValues, 
+    optionsRegions, 
+    optionsRoles, 
+    optionsDisponiblities 
+  } from '../api/filter'
 
 import backgroundImage from '../images/teamSignup-background.jpg'
 import styled from 'styled-components'
@@ -45,30 +52,56 @@ const InputContainer = styled.div`
   top: 30%;
   right: 20%;
 `
+const SelectStyled = styled.div`
+  color: black;
+`
 
 const TeamCreation = () => {
+    const [optionsRegion, setOptionsRegion] = useState([])
+    const [selectedRegion, setSelectedRegion] = useState([])
+    const [optionsLanguages, setOptionsLanguages] = useState([])
+    const [selectedLanguages, setSelectedLanguages] = useState([])
+    const [selectedDisponiblities, setSelectedDisponiblities] = useState([])
 
-  const formik = useFormik({
-    initialValues: {
-      name: ""
-    },
-    onSubmit: values => {
-      console.log(values)
-    }
-  })
+    // useEffect( async () => {
+    //     setSelectedRegion([{ label: user.region, value: user.region }])
+    //     const dataRegion = await getRegion()
+    //     setOptionsRegion(dataRegion)
+    //     const dataLanguages = await getLanguages()
+    //     setOptionsLanguages(dataLanguages)
+    //     const userLanguages = getSelectedInfos(user.languages)
+    //     setSelectedLanguages(userLanguages)
+    //     const userDisponibilities = getSelectedInfos(user.disponibilities)
+    //     setSelectedDisponiblities(userDisponibilities)
+    //     const userRoles = getSelectedInfos(user.roles)
+    //     setSelectedRoles(userRoles)
+    // },[])
 
-  fetch('http://localhost:5000/teams', {
-    method: 'post',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    credentials: 'include',
-    body: JSON.stringify({
-      // name: values.name,
+    const formik = useFormik({
+        initialValues: {
+        name: ""
+        },
+        onSubmit: values => {
+        console.log(values)
+        }
     })
-  })
 
-  console.log(formik.values)
+    fetch('http://localhost:5000/teams', {
+        method: 'post',
+        headers: {
+        'Content-type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify({
+        // name: values.name,
+        })
+    })
+
+    console.log(formik.values)
+    console.log(selectedLanguages)
+    console.log(selectedRegion)
+    console.log(selectedDisponiblities)
+
 
   return (
     <>
@@ -92,16 +125,54 @@ const TeamCreation = () => {
     </SideDiv>
     <InputContainer>
 
-      <form onSubmit={formik.handleSubmit}>
-        <input
-          name="name"
-          type="text"
-          placeholder="Name"
-          value={formik.values.name}
-          onChange={formik.handleChange}
-        />
-        <button type="submit">Submit</button>
-      </form>
+        <p>Modifier votre région :</p>
+        <SelectStyled>
+            <Select 
+            value={selectedRegion}
+            options={optionsRegions} 
+            onChange={setSelectedRegion}
+            />
+            {/* <MultiSelect
+            options={optionsRegion}
+            value={selectedRegion}
+            onChange={setSelectedRegion}
+            labelledBy="Select"
+            hasSelectAll={false}
+            /> */}
+        </SelectStyled>
+
+        <p>Choisissez votre langue :</p>
+        <SelectStyled>
+            <MultiSelect
+            options={optionsLanguages}
+            value={selectedLanguages}
+            onChange={setSelectedLanguages}
+            labelledBy="Select"
+            hasSelectAll={false}
+            />
+        </SelectStyled>
+
+        <p>Indiquer vos disponiblitiés :</p>
+        <SelectStyled>
+            <MultiSelect
+            options={optionsDisponiblities}
+            value={selectedDisponiblities}
+            onChange={setSelectedDisponiblities}
+            labelledBy="Select"
+            hasSelectAll={false}
+            />
+        </SelectStyled>
+
+        <form onSubmit={formik.handleSubmit}>
+            <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            />
+            <button type="submit">Submit</button>
+        </form>
 
     </InputContainer>
     </Header>
