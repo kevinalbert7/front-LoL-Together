cbfea6526f022e8e32df0c8f416bc23238f6f61f
kevinalbert7 <20ko@live.fr>
2022-02-07 12:15:06 +0100

    footer termine

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { RiTwitterLine, RiFacebookLine, RiInstagramLine, RiGithubLine } from 'react-icons/ri'
+import { RiTwitterLine, RiFacebookLine, RiInstagramLine, RiGithubLine, RiTwitchFill } from 'react-icons/ri'
 
 import styled from 'styled-components'
 
@@ -14,6 +14,48 @@ const FooterBackgroundRow = styled.div`
   display: flex;
   justify-content: space-around;
 `
+const IconsList = styled.div`
+ul {
+  display: flex;
+  align-items: center;
+  margin-top: 15px;
+}
+ul li {
+    display: block;
+    background: black;
+    border-radius: 20%;
+    margin: 10px;
+    list-style-type: none;
+    font-size: 30px;
+    transition: 0.5s;
+  }
+  ul li a {
+    height: 60px;
+    width: 60px;
+    padding: 4px 0px 0px 16px;
+    display: block;
+    color: #fff;
+  }
+  ul li:nth-child(1) {
+    background: #1DA1F2 ;
+  }
+  ul li:nth-child(2) {
+    background: #E4405F ;
+  }
+  ul li:nth-child(3) {
+    background: #1877F2 ;
+  }
+  ul li:nth-child(4) {
+    background: #6441A4;
+  }
+  ul li:nth-child(5) {
+    background: #4078c0  ;
+  }
+  ul li:hover {
+    transform: translateY(-8px);
+    box-shadow: 7px 7px rgba(255, 255, 255, .7);
+  }
+`
 
 const Footer = () => {
   return (
@@ -25,12 +67,15 @@ const Footer = () => {
       </FooterBackgroundRow>
       <div className='d-flex align-items-center my-5 flex-column'>
         <Title text='Made by LoL Together Team' size='18' />
-        <div className='d-flex justify-content-center my-3'>
-          <RiTwitterLine  color="white" size="3%"/>
-          <RiFacebookLine  color="white" size="3%"/>
-          <RiInstagramLine  color="white" size="3%"/>
-          <RiGithubLine  color="white" size="3%"/>
-        </div>
+        <IconsList>
+          <ul>
+            <li><a href="https://twitter.com/LeagueOfLegends"><RiTwitterLine /></a></li>
+            <li><a href="https://www.instagram.com/leagueoflegends/?hl=fr"><RiInstagramLine /></a></li>
+            <li><a href="https://www.facebook.com/leagueoflegends/"><RiFacebookLine /></a></li>
+            <li><a href="https://www.twitch.tv/directory/game/League%20of%20Legends"><RiTwitchFill  /></a></li>
+            <li><a href="https://github.com/JongCHONG"><RiGithubLine /></a></li>
+          </ul>
+        </IconsList>
       </div>
     </FooterBackground>
   )
