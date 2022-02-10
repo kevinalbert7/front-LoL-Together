77faaf764dd8503595bc52cf8bf39338263c022b
kevinalbert7 <20ko@live.fr>
2022-02-11 00:01:16 +0100

    home texte

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -86,7 +86,7 @@ const Home = () => {
                   <p>Vous cherchez un joueur?</p>
                 </HomeCard>
                 <Description>
-                  Ici, vous aurez la possibilité de recruter un joueur selon sa langue, son rôle, la région dans laquelle il joue ou même selon les horaires auxquelles il joue ndard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </Description>
                 <div className='row'>
                   <div className='col-4'>
